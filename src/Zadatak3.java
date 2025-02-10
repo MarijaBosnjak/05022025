2e64d8a40d32b60a3fe267dc897ff6bd723356cc
MarijaBosnjak <mery999000@gmail.com>
2025-02-10 17:13:53 +0100

    Zadatak3 Dz

--- src/Zadatak3.java
+++ src/Zadatak3.java
@@ -5,8 +5,25 @@
 //Primjer izlaza: svima dan Dobar
 
 
+import java.util.Scanner;
+
 public class Zadatak3 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesi zeljenu recenicu ");
+        String recenica = sc.nextLine();
+
+        String[] rijeci = recenica.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = rijeci.length - 1; i >= 0; i--) {
+            sb.append(rijeci[i]);
+            if (i > 0) {
+                sb.append(" ");
+            }
+            System.out.println("Obrnuti redosljed rijeci je: " +sb.toString());
+        }
     }
+
 }
